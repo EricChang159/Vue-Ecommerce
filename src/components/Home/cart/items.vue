bdac541a46ec7ebde2adfe069c83299bedd20cf5
ERIC <eric.chang159@gmail.com>
2019-08-07 19:14:37 +0800

    cart icon fix

--- src/components/Home/cart/items.vue
+++ src/components/Home/cart/items.vue
@@ -5,11 +5,11 @@
 -->
         
         <div class="container-fluid bg-light">
-            <!-- <button @click="getsomething">cartData</button> -->
+            <button @click="getsomething">cartData</button>
             <router-view  />
             <router-link to="/shoppingcenter/cart">
                 <div class="add-to-cart" @click="addItemsToCart">
-                    <img :src="cartIcon">
+                    <img :src="location.href + cartIcon">
                     <button class="btn btn-outline-primary" type="button">Add to cart</button>
                 </div>
             </router-link>
@@ -161,7 +161,8 @@
         },
         methods: {
             getsomething(){
-                console.log(this.cartData)
+                // console.log(this.cartData)
+                console.log(location.href)
             },
             showMore(way){
                if(way>0){
